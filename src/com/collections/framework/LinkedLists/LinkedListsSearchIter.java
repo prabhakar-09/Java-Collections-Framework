9ad096e163d6569b69486f9832a8175a912df869
Prabhakar <kulkarniprabhakar2000@gmail.com>
2024-02-22 23:56:02 +0530

    Intro to basic method operations on LL

---
+++ src/com/collections/framework/LinkedLists/LinkedListsSearchIter.java
@@ -0,0 +1,26 @@
+package com.collections.framework.LinkedLists;
+import java.util.*;
+
+public class LinkedListsSearchIter {
+
+	public static void main(String [] args) {
+		
+		LinkedList<String> languages = new LinkedList<>();
+		
+		languages.add("Hindi");
+		languages.add("English");
+		languages.add("Kannada");
+		languages.add("Telugu");
+		languages.add("Tamil");
+		languages.add("Telugu");
+
+		
+		System.out.println(languages.contains("Hindi")); // Returns true if exists.
+		
+//		Finding index of first occurrence in a linkedList
+		System.out.println(languages.indexOf("Telugu")); // returns 3
+		
+//		Finding elements of last occurrences of elements
+		System.out.println(languages.lastIndexOf("Telugu")); // returns 5
+	}
+}
