098a17692e3460b10fefa69f036a272d014d8adc
Prabhakar <kulkarniprabhakar2000@gmail.com>
2024-02-24 12:01:09 +0530

    Demo class for comparison of Arr List & Linked Lits

---
+++ src/com/collections/framework/LinkedLists/ArrListVsLinkedList.java
@@ -0,0 +1,55 @@
+package com.collections.framework.LinkedLists;
+
+// A simple comparison class for Arr List & Linked lists
+public class ArrListVsLinkedList {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
+
+//concise comparison between ArrayList and LinkedList:
+
+/*
+ * Implementation:
+ * 
+ * ArrayList: Resizable array implementation. 
+ * LinkedList: Doubly linked list implementation. 
+ * 
+ * Memory Allocation:
+ * 
+ * ArrayList: Contiguous memory allocation. L
+ * inkedList: Dynamic memory allocation for nodes. 
+ * 
+ * Access Time:
+ * 
+ * ArrayList: Faster for random access (O(1)). 
+ * LinkedList: Slower for random access (O(n)). 
+ * 
+ * Insertions and Deletions:
+ * 
+ * ArrayList: Slower for insertions and deletions in middle (O(n)). 
+ * LinkedList: Faster for insertions and deletions (O(1)) if position known. 
+ * 
+ * Memory Overhead:
+ * 
+ * ArrayList: Lower memory overhead. 
+ * LinkedList: Higher memory overhead due to node structure. 
+ * 
+ * Traversal:
+ * 
+ * ArrayList: Faster for sequential access. 
+ * LinkedList: Slower for sequential access due to pointer traversal. 
+ * 
+ * Usage:
+ * 
+ * ArrayList: Suitable for frequent access and read-only operations. 
+ * LinkedList: Suitable for frequent insertions and deletions.
+ */
+
+
+
+
