efec3c097d947eeb77ae9533ceb0688c2b12fed9
Prabhakar <kulkarniprabhakar2000@gmail.com>
2024-03-16 10:16:29 +0530

    Syncronized array list access

---
+++ src/com/collections/framework/z_practice/SyncAccessOfArrLists.java
@@ -0,0 +1,13 @@
+package com.collections.framework.z_practice;
+
+/*The ArrayList class is not synchronized. If multiple threads try to modify an 
+ArrayList at the same time then the final result becomes not-deterministic because 
+one thread might override the changes done by another thread.*/
+
+public class SyncAccessOfArrLists {
+
+	public static void main(String[] args) {
+		
+		
+	}
+}
